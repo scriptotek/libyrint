344b6e48bf047ed57b882cf2e819d58ed9546975
Dan Michael O. Heggø <danmichaelo@gmail.com>
2019-09-26 14:37:49 +0200

    Add entry page for standard setup
    The modules are independent, but it's good to have a standard setup that
    can be run out of the box.

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Libyrint</title>
+</head>
+<body>
+  <ul>
+    <li>
+      <a href="/client/">Kartvisning</a>
+    </li>
+    <li>
+      <a href="/editor/">Karteditor</a>
+    </li>
+  </ul>
+</body>
+</html>
